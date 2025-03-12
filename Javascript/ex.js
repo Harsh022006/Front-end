d2675a84ecfa6cfcf6cb59167ef59869e695e996
Harsh022006 <harshamburkar@gmail.com>
2025-03-12 17:03:59 +0530

    assignment

---
+++ Javascript/ex.js
@@ -0,0 +1,5 @@
+console.log("hello javascript")
+document.getElementById("h2").innerHTML = "heloo javascript"
+document.getElementById("h1").innerText = "hello mitra"
+
+console.log("kuchh nahi milega bhai")
